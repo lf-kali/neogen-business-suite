9868c95853b11cc8f0709bb75c9938bf0639dc27
lf-kali <cbjk.kali@gmail.com>
2026-01-11 20:28:00 -0300

    fix: dto visibility to swagger

--- src/device-brand/dto/create-device-brand.dto.ts
+++ src/device-brand/dto/create-device-brand.dto.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty, Length } from 'class-validator';
 
 export class CreateDeviceBrandDTO {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   @Length(1, 255)
--- src/device-brand/dto/update-device-brand.dto.ts
+++ src/device-brand/dto/update-device-brand.dto.ts
@@ -1,8 +1,5 @@
+import { PartialType } from '@nestjs/swagger';
 import { IsString, IsOptional, Length } from 'class-validator';
+import { CreateDeviceBrandDTO } from './create-device-brand.dto';
 
-export class UpdateDeviceBrandDTO {
-  @IsString()
-  @IsOptional()
-  @Length(1, 255)
-  name?: string;
-}
+export class UpdateDeviceBrandDTO extends PartialType(CreateDeviceBrandDTO) {}
